4759db6bc44c593c63b14cd19856a99772d72302
Alexander Johnson <mr.johnson.alexander@gmail.com>
2021-06-05 14:19:58 -0700

    Enable honeycomb. apps.py API key needs adding

---
+++ vibe/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'vibe.apps.VibeConfig'
--- vibe/apps.py
+++ vibe/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+import beeline
 
 
 class VibeConfig(AppConfig):
     name = 'vibe'
+    def ready(self):
+        beeline.init(
+            writekey='',
+            dataset='vibes',
+            service_name='vibes',
+            debug=True,
+        )
